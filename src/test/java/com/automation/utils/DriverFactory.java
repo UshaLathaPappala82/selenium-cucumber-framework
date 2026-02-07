4360ba87094536c2ec64bdd44323f71aade2275a
ushalatha <ushalatha@192.168.1.131>
2026-02-07 14:34:29 -0500

    Commit three

--- src/test/java/com/automation/utils/DriverFactory.java
+++ src/test/java/com/automation/utils/DriverFactory.java
@@ -1,73 +1,45 @@
-package com.automation.utils;
-
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
+
 import io.github.bonigarcia.wdm.WebDriverManager;
 
 public class DriverFactory {
 
-    // Sequential driver
-    private static WebDriver driverStatic;
+    private static ThreadLocal<WebDriver> driver = new ThreadLocal<>();
 
-    // Parallel driver
-    private static ThreadLocal<WebDriver> driverThread = new ThreadLocal<>();
-
-    /**
-     * Get WebDriver instance
-     * @param parallel true if running parallel scenarios
-     * @return WebDriver
-     */
-    public static WebDriver getDriver(boolean parallel) {
-        String headless = System.getProperty("headless");
-
-        if (parallel) {
-            if (driverThread.get() == null) {
-                driverThread.set(createChromeDriver(headless));
-            }
-            return driverThread.get();
-        } else {
-            if (driverStatic == null) {
-                driverStatic = createChromeDriver(headless);
-            }
-            return driverStatic;
-        }
-    }
-
-    // Default method (sequential runs)
     public static WebDriver getDriver() {
-        return getDriver(false);
+        if (driver.get() == null) {
+            driver.set(createChromeDriver());
+        }
+        return driver.get();
     }
 
-    private static WebDriver createChromeDriver(String headless) {
+    private static WebDriver createChromeDriver() {
+
         WebDriverManager.chromedriver().setup();
+
         ChromeOptions options = new ChromeOptions();
         options.addArguments("--window-size=1920,1080");
         options.addArguments("--disable-gpu");
         options.addArguments("--no-sandbox");
         options.addArguments("--disable-dev-shm-usage");
-        if ("true".equalsIgnoreCase(headless)) {
+        options.addArguments("--remote-allow-origins=*");
+
+        boolean isCI = "true".equalsIgnoreCase(System.getenv("CI"));
+        boolean isHeadless = "true".equalsIgnoreCase(System.getProperty("headless"));
+
+        if (isCI || isHeadless) {
             options.addArguments("--headless=new");
         }
+
         return new ChromeDriver(options);
     }
 
-    public static void quitDriver(boolean parallel) {
-        if (parallel) {
-            if (driverThread.get() != null) {
-                driverThread.get().quit();
-                driverThread.remove();
-            }
-        } else {
-            if (driverStatic != null) {
-                driverStatic.quit();
-                driverStatic = null;
-            }
-        }
-    }
-
-    // Default quit (sequential)
     public static void quitDriver() {
-        quitDriver(false);
+        if (driver.get() != null) {
+            driver.get().quit();
+            driver.remove();
+        }
     }
 }
